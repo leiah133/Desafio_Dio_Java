effb7bc0dfd925843e0e5948de208c4feeb9e28b
Léia Oliver <leiaanjos87@gmail.com>
2024-05-09 17:35:11 -0300

    first commit

---
+++ SimulacaoBancaria.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        double saldo = 0; 
+        // Loop infinito para manter o programa em execução até que o usuário decida sair
+        while (true) { 
+         
+
+            int opcao = scanner.nextInt(); 
+        // TODO: Implemente as condições necessárias para avaliaa a opção escolhida:
+        // Dica: Utilze o switch/case para o programa realizar as operações escolhidas pelo usuário.
+            switch(opcao){
+              case 1 :
+                 
+                 double valorDepositado = scanner.nextDouble();
+                 saldo += valorDepositado;
+                  System.out.println(" Saldo atual: " + valorDepositado);
+                  break;
+              case 2 :
+                
+                  double valorSaque =  scanner.nextDouble();
+                  if (valorSaque <= saldo){
+                    saldo -= valorSaque;
+                    System.out.println("Saldo atual: " + valorSaque);
+                    
+                  }else{
+                     System.out.println("Saldo insuficiente.");
+                  }
+                  break;
+              case 3 :
+                 System.out.println("Saldo atual: " + saldo);
+                 break;
+              case 0 : 
+                 System.out.println("Programa encerrado.");
+                 System.exit(0);
+                break;
+            
+        
+        
+        // Exibe mensagem de opção inválida se o usuário escolher uma opção inválida:    
+                default:
+                    System.out.println("Opção inválida. Tente novamente."); 
+            }
+        }
+    }
+}
---
+++ VerificadorNumeroConta.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+public class VerificadorNumeroConta { 
+
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+          try{
+            String numeroConta = scanner.nextLine();
+            
+            verificarNumeroConta(numeroConta);
+            
+            System.out.println("Numero de conta valido."); 
+            
+          } catch(IllegalArgumentException e){
+            System.out.println("Erro: " + e.getMessage()); 
+            
+          } finally {
+            scanner.close();
+          }
+    }
+
+
+    private static void verificarNumeroConta(String numeroConta) { 
+
+      if (numeroConta.length() != 8) {
+       throw new IllegalArgumentException("Numero de conta invalido. Digite exatamente 8 digitos.");
+      }
+    }
+  }
